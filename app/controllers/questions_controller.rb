25e0fc21549e55d08f659edde9d7f7a04837c531
jerome balen <jerome.balen@yahoo.fr>
2024-11-26 11:13:41 +0100

    update

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,16 @@
 class QuestionsController < ApplicationController
-  
+
   def ask
   end
 
   def answer
+    @question = params[:question]
+    if @question == "I am going to work"
+      @answer = "Coach : Great !"
+    elsif @question.end_with?("?")
+      @answer = "Coach : SILLY QUESTION ! Get dressed and go to work !"
+    else
+      @answer = "Coach : I don't care ! Get dressed and go to work !"
+    end
   end
-
 end
